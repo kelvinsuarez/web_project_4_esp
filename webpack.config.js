3ee2891eafe9d6ed80d7fb4da538ebce4be46f41
Kelvin Suarez <k_suarez22@hotmail.com>
2024-02-08 11:24:37 -0600

    proyecto empaquetado completo

--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,11 @@ module.exports= {
             use: [
               MiniCssExtractPlugin.loader,
               {
-                loader: "css-loader"
+                loader: "css-loader",
+                options: { importLoaders: 1 }
               },
-            ]
+              "postcss-loader"
+            ],
           },
           {
             // la regla para procesar archivos
